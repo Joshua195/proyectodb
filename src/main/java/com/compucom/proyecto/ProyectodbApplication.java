0776833008b280aa644b1bc134354c0c92950fb0
Josue Gonzalez <gojoshua195@gmail.com>
2017-10-05 08:30:41 -0500

    initial commit

---
+++ src/main/java/com/compucom/proyecto/ProyectodbApplication.java
@@ -0,0 +1,12 @@
+package com.compucom.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectodbApplication.class, args);
+	}
+}
